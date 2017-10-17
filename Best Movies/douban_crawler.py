91184780d7e1dd3b72af2f13accd82f7eef963bb
Andy Li <li.chenxing@gmail.com>
2017-10-17 20:50:54 +0800

    update Best Movies

--- Best Movies/douban_crawler.py
+++ Best Movies/douban_crawler.py
@@ -8,8 +8,8 @@ def get_movie_url(category, location):
     return f'{base_url}{category},{location}'
 
 def parse_movie(category, location, item):
-    name = item.find('span', class_='title').string
-    rate = item.find('span', class_='rate').string
+    name = item.find('span', 'title').string
+    rate = item.find('span', 'rate').string
 
     info_link = item['href']
     cover_link = item.find('img')['src']
@@ -18,11 +18,11 @@ def parse_movie(category, location, item):
 
 def get_movies(category, location):
     url = get_movie_url(category, location)
-    html = expanddouban.getHtml(url)
+    html = expanddouban.getHtml(url, loadmore=True)
 
     soup = BeautifulSoup(html, 'lxml')
-    movies_list = soup.find('div', class_='list-wp')
-    movies_items = movies_list.find_all('a', class_='item')
+    movies_list = soup.find('div', 'list-wp')
+    movies_items = movies_list.find_all('a', 'item')
 
     yield from (parse_movie(category, location, item) 
                 for item in movies_items)
@@ -32,17 +32,16 @@ def get_categories_locations():
     html = expanddouban.getHtml(url)
 
     soup = BeautifulSoup(html, 'lxml')
-    uls = soup.find_all('ul', class_='category')
+    uls = soup.find_all('ul', 'category')
 
-    categories_ul = uls[1]
-    categories_lis = categories_ul.find_all('li')
+    categories_lis = uls[1].find_all('li')
     categories = [li.string for li in categories_lis]
 
-    locations_ul = uls[2]
-    locations_lis = locations_ul.find_all('li')
+    locations_lis = uls[2].find_all('li')
     locations = [li.string for li in locations_lis]
     
     # skip '全部类型' and '全部地区'
+    # I thought pop() pops out the last item of the list?
     return categories[1:], locations[1:]
 
 class Movie:
@@ -57,10 +56,10 @@ class Movie:
 if __name__ == '__main__':
     categories, locations = get_categories_locations()
 
-    movie_lists = (get_movies(category, location)
-                  for category, location in product(categories, locations))
+    movies_lists = (get_movies(category, location)
+                    for category, location in product(categories, locations))
 
-    movies = chain.from_iterable(movie_lists)
+    movies = chain.from_iterable(movies_lists)
 
     movies_df = pd.DataFrame([m.__dict__ for m in movies])
     movies_df = movies_df[['name', 'rate', 'location', 'category', 'info_link', 'cover_link']]
@@ -75,6 +74,7 @@ if __name__ == '__main__':
 
             top3 = count.loc[category].sort_values(ascending=False)[:3]
             subtotal = count.loc[category].sum()
+            # top3 is actually a pandas Series, which supports easy scalar operation :)
             percentages = top3 / subtotal
 
             for location, percentage in percentages.iteritems():
